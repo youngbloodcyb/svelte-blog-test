519277de9ecddf77a8e133e2e3ea2b5a775f569f
Cameron Youngblood <cameronyoungblood@camerons-air-4.lan>
2022-05-10 23:22:03 -0600

    initial commit

---
+++ src/routes/blog/[id].svelte
@@ -0,0 +1,51 @@
+<script context="module">
+    export const load = async ({fetch, params}) => {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+
+        const post = await res.json();
+        
+        return {
+            props: {
+                post,
+            },
+        };
+    };
+</script>
+
+<script>
+    export let post; 
+</script>
+<a href={"/"}>
+    <img src="/images/cat.jpg" id="cat">
+</a>
+<h2>{post.title}</h2>
+<p>{post.body}</p>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Space+Mono&display=swap');
+    
+    * {
+    font-family: 'Space Mono', monospace;
+    }
+
+    h1 {
+    font-size: 48px;
+    }
+
+    h2 {
+        font-size: 18px;
+        margin: 0;
+        text-align: center;
+    }
+
+    p {
+        font-size: 14px;
+    }
+
+    #cat {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        margin-bottom: 40px;
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,73 @@
+<script context="module">
+    export const load = async({fetch}) => {
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const posts = await res.json();
+        return {
+            props: {
+                posts,
+            }
+        }
+    }
+</script>
+
+<script>
+    export let posts
+</script>
+
+<img src="/images/cat.jpg" id="cat">
+
+<h1>posts</h1>
+
+<div class="posts">
+    {#each posts as item}
+        <div class="post">
+            <h2>{item.title.substring(0, 20)}...</h2>
+            <p>{item.body.substring(0, 80)}...</p>
+            <p class="link">
+                <a href={`/blog/${item.id}`}>Read More</a>
+            </p>
+        </div>
+    {/each}
+</div>
+
+<style global>
+    @import url('https://fonts.googleapis.com/css2?family=Space+Mono&display=swap');
+    
+    * {
+    font-family: 'Space Mono', monospace;
+    }
+
+    h1 {
+    font-size: 48px;
+    text-align: center;
+    }
+
+    h2 {
+        font-size: 18px;
+        margin: 0;
+    }
+
+    p {
+        font-size: 14px;
+    }
+
+    .posts {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 20px;
+    }
+
+    .post {
+        
+    }
+
+    .link {
+        text-align: right;
+    }
+
+    #cat {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
